c671887c7f62f2dc8e05abbce1613a6a3adcc36e
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-09-22 13:48:47 -0300

    feat: movie model impl

--- src/main/java/com/phsushi/Moviespeed/model/Movie.java
+++ src/main/java/com/phsushi/Moviespeed/model/Movie.java
@@ -1,4 +1,82 @@
 package com.phsushi.Moviespeed.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tblMovie")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_movie;
+    private String title;
+    private Float rating;
+    private String director;
+    private String synopsis;
+    private LocalDate realeaseDate;
+
+    public Movie() {
+    }
+
+    public Movie(Long id_movie, String title, Float rating, String director, String synopsis, LocalDate realeaseDate) {
+        this.id_movie = id_movie;
+        this.title = title;
+        this.rating = rating;
+        this.director = director;
+        this.synopsis = synopsis;
+        this.realeaseDate = realeaseDate;
+    }
+
+    public Long getId_movie() {
+        return id_movie;
+    }
+
+    public void setId_movie(Long id_movie) {
+        this.id_movie = id_movie;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public LocalDate getRealeaseDate() {
+        return realeaseDate;
+    }
+
+    public void setRealeaseDate(LocalDate realeaseDate) {
+        this.realeaseDate = realeaseDate;
+    }
+
+
+
+
 }
