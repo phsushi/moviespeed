4cac5d3e0451a7bd53945d7186988aab55a9dca9
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-09-22 15:59:35 -0300

    feat: delete movie route

--- src/main/java/com/phsushi/Moviespeed/controller/MovieController.java
+++ src/main/java/com/phsushi/Moviespeed/controller/MovieController.java
@@ -27,4 +27,8 @@ public class MovieController {
         return movieService.createMovie(movie);
    }
 
+   @DeleteMapping("/{id}")
+    public void deleteMovie(@RequestBody Long id){
+        movieService.deleteMovie(id);
+   }
 }
