cfad494bee9e661c797c3a3ec191db240ff9d7e3
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-09-22 15:19:43 -0300

    feat: list all movies route & create movie route

--- src/main/java/com/phsushi/Moviespeed/controller/MovieController.java
+++ src/main/java/com/phsushi/Moviespeed/controller/MovieController.java
@@ -1,4 +1,30 @@
 package com.phsushi.Moviespeed.controller;
 
+import com.phsushi.Moviespeed.model.Movie;
+import com.phsushi.Moviespeed.service.MovieService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/moviespeed")
 public class MovieController {
+
+   private final MovieService movieService;
+
+   MovieController(MovieService movieService){
+       this.movieService = movieService;
+   }
+
+
+   @GetMapping
+   public List<Movie> getAllMovies(){
+       return movieService.getAllMovies();
+   }
+
+   @PostMapping
+    public Movie createMovie(@RequestBody Movie movie){
+        return movieService.createMovie(movie);
+   }
+
 }
