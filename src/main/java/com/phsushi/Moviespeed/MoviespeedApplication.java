d7f770a6211df9bfdca0bd326bb56a9d59e28ba6
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-09-22 13:23:12 -0300

    initial commit

---
+++ src/main/java/com/phsushi/Moviespeed/MoviespeedApplication.java
@@ -0,0 +1,13 @@
+package com.phsushi.Moviespeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviespeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviespeedApplication.class, args);
+	}
+
+}
