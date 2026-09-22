64084359d05496cfb338e95a3a14ea116321d3ce
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-09-22 15:20:46 -0300

    feat: CRD service impl

--- src/main/java/com/phsushi/Moviespeed/service/MovieService.java
+++ src/main/java/com/phsushi/Moviespeed/service/MovieService.java
@@ -1,4 +1,28 @@
 package com.phsushi.Moviespeed.service;
 
+import com.phsushi.Moviespeed.model.Movie;
+import com.phsushi.Moviespeed.repository.MovieRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MovieService {
+    private final MovieRepository movieRepository;
+
+    MovieService(MovieRepository movieRepository){
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> getAllMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Movie createMovie(Movie movie){
+        return movieRepository.save(movie);
+    }
+
+    public void deleteMovie(Long id){
+        movieRepository.deleteById(id);
+    }
 }
